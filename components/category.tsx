93157f679107bade7904e6c1b333c23f22f1c30e
adibfiroz <adibfiroz9702@gmail.com>
2025-03-09 20:19:54 +0530

    merge

--- components/category.tsx
+++ components/category.tsx
@@ -12,9 +12,6 @@ interface SliderItemProps {
 }
 
 const SliderItem: React.FC<SliderItemProps> = ({ label }) => {
-    const handleCollectionRoute = () => {
-        window.location.href = `/search/${label}`
-    }
 
     return (
         <Link href={`/search/${label}`} className={cn("px-6 py-3 bg-white whitespace-nowrap border rounded-md shadow-sm hover:bg-teal-500/20 hover:text-teal-800 hover:border-teal-500/20 cursor-pointer transition-all duration-300 lowercase text-[16px]")}>
